649500c4b1133c88025883c975af0490b2658971
Gaboso <gabrielsantiago2@gmail.com>
2019-11-12 16:44:58 -0300

    Remoção do brand

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Brand } from 'components';
 import { GlobalStyle } from 'core/assets/style/global';
 import logo from 'core/assets/images/logo.svg';
 import { AppHeader, AppContainer } from './styled';
@@ -12,7 +11,6 @@ class App extends PureComponent {
       <Fragment>
         <AppContainer>
           <AppHeader>
-            <Brand brandImage={logo} />
             <p>Initial project structure</p>
             <Link to="/customers">Customers Page</Link>
           </AppHeader>
